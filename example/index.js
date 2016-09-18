a135f53500acfd54a2d7df961ddd33212e38507b
Ionică Bizău <bizauionica@gmail.com>
2016-09-18 21:38:10 +0300

    Fancy output :rocket:

--- example/index.js
+++ example/index.js
@@ -1,6 +1,14 @@
 "use strict";
 
-const threeABN = require("../lib")
+const ThreeABN = require("../lib")
 
-// Start playing
-threeABN();
+ThreeABN.playlistInfo((err, data) => {
+    if (err) { return console.error(err); }
+    console.log(data.playlist.map(c => `${c.date.format("HH:mm")} - ${c.title}`).join("\n"));
+
+    // Start playing
+    let radio = new ThreeABN({ autoplay: false });
+    radio.on("song_changed", (err, current, latestFive, playlist) => {
+        console.log("Playing: " + current);
+    });
+});
